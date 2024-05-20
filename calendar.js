e68fcb62688a80dd6d85f014c8f4bcb330c17ed0
Joy <jungheeyoo.joy@gmail.com>
2024-05-21 01:08:31 +0900

    세 번째 커밋

--- calendar.js
+++ calendar.js
@@ -26,11 +26,18 @@ const months = [
 ];
 
 const renderCalendar = () => {
+  // 1일 시작 요일,
   const firstDay = new Date(currentYear, currentMonth, 1).getDay();
   console.log(firstDay);
+  // 이번 달 마지막 날짜, 요일
   const lastDate = new Date(currentYear, currentMonth + 1, 0).getDate();
   const lastDay = new Date(currentYear, currentMonth, lastDate).getDay();
+  console.log(lastDate);
+  console.log(lastDay);
+  // 지난 달 마지막 날짜
   const lastDayOfLastMonth = new Date(currentYear, currentMonth, 0).getDate();
+  console.log(lastDayOfLastMonth);
+
   const navMonth = document.querySelector('.nav-month');
   const navYear = document.querySelector('.nav-year');
 
@@ -41,12 +48,35 @@ const renderCalendar = () => {
   const dates = document.querySelector('.dates');
   dates.innerHTML = '';
 
-  //현재달에 이전달 불러오기
+  //현재 달에 이전 달 불러오기
   for (let i = firstDay; i > 0; i--) {
-    const $day = document.createElement('li');
-    $day.classList.add('notCurrDay');
-    $day.innerText = lastDayOfLastMonth - i + 1;
-    dates.append($day);
+    const day = document.createElement('div');
+    day.classList.add('pre-date');
+    day.innerText = lastDayOfLastMonth - i + 1;
+    dates.append(day);
+  }
+
+  //현재 달 만들기
+  for (let i = 1; i <= lastDate; i++) {
+    const day = document.createElement('div');
+    day.innerText = i;
+    day.classList.add('cur-date');
+    dates.append(day);
+    //오늘 표시
+    if (
+      i - firstDay + 1 === date.getDate() &&
+      currentMonth === date.getMonth()
+    ) {
+      day.classList.add('today');
+    }
+  }
+
+  //현재달에 다음달 불러오기
+  for (let i = lastDay; i < 6; i++) {
+    const day = document.createElement('div');
+    day.classList.add('next-date');
+    day.innerText = i - lastDay + 1;
+    dates.append(day);
   }
 };
 
